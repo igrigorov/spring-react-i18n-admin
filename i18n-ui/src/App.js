e386208d24f155e0e5c901ab2af56ae7dfb4591e
Ivan Grigorov <igrigorov@cognity.gr>
2021-04-13 17:40:56 +0300

    ~ reformat all UI code

--- i18n-ui/src/App.js
+++ i18n-ui/src/App.js
@@ -2,27 +2,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+	return (
+		<div className="App">
+			<header className="App-header">
 
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Internationalization Administration
-        </a>
+				<img src={logo} className="App-logo" alt="logo"/>
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Internationalization Administration
+				</a>
 
-      </header>
+			</header>
 
-    </div>
-  );
+		</div>
+	);
 }
 
 export default App;
