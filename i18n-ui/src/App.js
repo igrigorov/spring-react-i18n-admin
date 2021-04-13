ed099e303b2f8e014c8e29e07fa356ffc9fac0b6
lambrospap <43785709+lambrospap@users.noreply.github.com>
2021-04-13 11:11:14 +0300

    Update i18n-ui/src/App.js
    Co-authored-by: Ivan Grigorov <grigorov.ivan@gmail.com>

--- i18n-ui/src/App.js
+++ i18n-ui/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Internationalization Administration
         </a>
 
       </header>
