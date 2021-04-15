70c59a287409b0978b66197e416cc0425555cf97
Labros Papadopoulos <lapapadopoulos@cognity.gr>
2021-04-15 16:59:55 +0300

    Localization Administration Page initialize

--- i18n-ui/src/App.js
+++ i18n-ui/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Sample from "./view";
+import {Switch} from "@material-ui/core";
+import React from 'react'
+
 
 function App() {
 	return (
 			<div className="table">
+				<h1 align="center">Internationalization Administration</h1>
 				<Sample/>
 			</div>
 
--- i18n-ui/src/view/tableThreshold.js
+++ i18n-ui/src/view/tableThreshold.js
@@ -1,40 +1,139 @@
 import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import {Switch} from "@material-ui/core";
+import {DataGrid} from '@material-ui/data-grid';
+import {FormControlLabel, Switch, TextField} from "@material-ui/core";
+import {Component, useState} from "react";
+import MaterialTable from "material-table";
+import {TextFields} from "@material-ui/icons";
 
-const columns = [
-	{ field: 'id', headerName: 'ID', width: 70 },
-	{ field: 'firstName', headerName: 'First name', width: 130 },
-	{ field: 'lastName', headerName: 'Last name', width: 130 },
+
+
+let rows = [
 	{
-		field: 'age',
-		headerName: 'Age',
-		type: 'number',
-		width: 90,
+		active: true,
+		lic: "test",
+		BG: "София",
+		EN: "Sofia"
 	},
 	{
-		field: 'fullName',
-		headerName: 'Full name',
-		description: 'This column has a value getter and is not sortable.',
-		sortable: false,
-		width: 160,
-		valueGetter: (params) =>
-			`${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+		active: false,
+		lic: "test2",
+		BG: "София",
+		EN: "Sofia"
+	}, {
+		active: true,
+		lic: "test",
+		BG: "София",
+		EN: "Sofia"
 	},
+	{
+		active: false,
+		lic: "test2",
+		BG: "София",
+		EN: "Sofia"
+	}, {
+		active: true,
+		lic: "test",
+		BG: "София",
+		EN: "Sofia"
+	},
+	{
+		active: false,
+		lic: "test2",
+		BG: "София",
+		EN: "Sofia"
+	}, {
+		active: true,
+		lic: "test",
+		BG: "София",
+		EN: "Sofia"
+	},
+	{
+		active: false,
+		lic: "test2",
+		BG: "София",
+		EN: "Sofia"
+	}, {
+		active: true,
+		lic: "test",
+		BG: "София",
+		EN: "Sofia"
+	},
+	{
+		active: false,
+		lic: "test2",
+		BG: "София",
+		EN: "Sofia"
+	}, {
+		active: true,
+		lic: "test",
+		BG: "София",
+		EN: "Sofia"
+	},
+	{
+		active: false,
+		lic: "test2",
+		BG: "София",
+		EN: "Sofia"
+	},];
+
+
+/*
+const changeRow = (oldRow, e) => {
+	const changeData = { [e.target.name]: e.target.checked };
+	const newRow = { ...oldRow, ...changeData };
+
+	console.log(oldRow, e);
+	const prvSt=this.state.changed;
+	const index = rows.findIndex(dtaRow => dtaRow.id === oldRow.id);
+	const newData = rows;
+	newData[index] = newRow;
+	this.setState({changed: !oldRow.active})
+	console.log(newData);
+};
+*/
+
+
+
+
+let columns = [
+	{
+		title: "Active",
+		field: "active",
+		render: (data) => (
+			<Switch
+				checked={data.active}
+				//onChange={handleChange}
+				name="active"
+				color="primary"
+			/>
+		)
+	},
+	{title: "lic", field: "lic"},
+	{
+		title: "BG", field: "BG", render: (data) => (
+			<TextField defaultValue={data.BG}/>)
+	},
+	{
+		title: "EN", field: "EN", render: (data) => (
+			<TextField defaultValue={data.EN}/>)
+	}
 ];
 
-const rows = [
-	{ id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-	{ id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-	{ id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-	{ id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-];
 
-export default function DataTable() {
-	return (
-		<div style={{ height: 400, width: '100%' }}>
-			<DataGrid rows={rows} columns={columns}  pageSize={5} checkboxSelection />
 
-		</div>
-	);
+export default class Sample extends Component {
+
+	render() {
+		return (
+			<MaterialTable
+				columns={columns}
+				data={rows}
+				title={""}
+				options={{
+					search: true,
+					paging: false,
+					tableLayout: "auto",
+					maxBodyHeight: 580,
+				}}/>)
+	}
 }
