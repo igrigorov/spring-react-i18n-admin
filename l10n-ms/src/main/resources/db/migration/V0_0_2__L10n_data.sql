babc1e5a9d581ff698900fe6fac55893a66b341a
Labros Papadopoulos <lapapadopoulos@cognity.gr>
2021-04-23 17:27:12 +0300

    Localization Administration Page #4 - Api calls separate files - update

--- l10n-ms/src/main/resources/db/migration/V0_0_2__L10n_data.sql
+++ l10n-ms/src/main/resources/db/migration/V0_0_2__L10n_data.sql
@@ -5,5 +5,22 @@ VALUES (L10n_id_seq.nextval, 'EN', 'test.test', 'test'),
        (L10n_id_seq.nextval, 'EN', 'test.first', 'first'),
        (L10n_id_seq.nextval, 'BG', 'test.first', 'първи'),
        (L10n_id_seq.nextval, 'EN', 'test.second', 'second'),
-       (L10n_id_seq.nextval, 'BG', 'test.second', 'втори');
+       (L10n_id_seq.nextval, 'BG', 'test.second', 'втори'),
+       (L10n_id_seq.nextval, 'EN', 'sample.checkBox', 'check box'),
+       (L10n_id_seq.nextval, 'BG', 'sample.checkBox', 'отметка'),
+       (L10n_id_seq.nextval, 'EN', 'sample.title', 'title'),
+       (L10n_id_seq.nextval, 'BG', 'sample.title', 'титла'),
+       (L10n_id_seq.nextval, 'EN', 'sample.plainText', 'Plain text'),
+       (L10n_id_seq.nextval, 'BG', 'sample.plainText', 'обикновен текст'),
+       (L10n_id_seq.nextval, 'EN', 'sample.button', 'Button'),
+       (L10n_id_seq.nextval, 'BG', 'sample.button', 'бутон'),
+       (L10n_id_seq.nextval, 'EN', 'sample.textArea', 'Text Area'),
+       (L10n_id_seq.nextval, 'BG', 'sample.textArea', 'Текстова област'),
+       (L10n_id_seq.nextval, 'EN', 'sample.radioButton', 'Radio Button'),
+       (L10n_id_seq.nextval, 'BG', 'sample.radioButton', 'Радио бутон'),
+       (L10n_id_seq.nextval, 'EN', 'sample.comboBox', 'Combo box'),
+       (L10n_id_seq.nextval, 'BG', 'sample.comboBox', 'Комбинирана кутия'),
+       (L10n_id_seq.nextval, 'EN', 'sample.switch', 'Switch'),
+       (L10n_id_seq.nextval, 'BG', 'sample.switch', 'Превключвател');
+
 
