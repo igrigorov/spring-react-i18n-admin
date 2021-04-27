4b65765736352782324d9d3760adaaceef32b4e5
Labros Papadopoulos <lapapadopoulos@cognity.gr>
2021-04-27 13:37:56 +0300

    Sample home page with i18n #2 - Flexes

--- i18n-ui/src/Views/Sample/index.js
+++ i18n-ui/src/Views/Sample/index.js
@@ -1,6 +1,23 @@
 import React, {Component} from 'react'
-import {Button} from "@material-ui/core";
+import {
+	Button,
+	FormControl,
+	FormHelperText,
+	InputLabel,
+	MenuItem,
+	NativeSelect,
+	Select,
+	TextareaAutosize,
+	TextField
+} from "@material-ui/core";
 import {Link} from "react-router-dom";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const options = [
+	'one', 'two', 'three'
+];
+const defaultOption = options[0];
 
 class SamplePage extends Component {
 
@@ -8,8 +25,75 @@ class SamplePage extends Component {
 		return (
 			<div>
 				<h1 align="center">Internationalization Administration</h1>
-				<Button><Link to="/Admin">Admin Page</Link></Button>
-			</div>)
+				<div style={{
+					display: 'flex', flexDirection: 'column', justifyContent: 'space-around',
+					alignItems: 'center', width: '100%', marginBottom: '20px'
+				}}>
+					<div style={{
+						display: 'flex', flexDirection: 'row', justifyContent: 'space-around',
+						alignItems: 'center', width: '100%', marginBottom: '20px'
+					}}>
+						<TextField label="Text Field" defaultValue="Medium" variant="outlined"/>
+						<TextField label="Text Field" defaultValue="Medium" variant="outlined"/>
+						<TextField label="Text Field" defaultValue="Medium" variant="outlined"/>
+					</div>
+					<div style={{
+						display: 'flex', flexDirection: 'row', justifyContent: 'space-around',
+						alignItems: 'center', width: '100%'
+					}}>
+						<TextField label="Text Field" defaultValue="Medium" variant="outlined"/>
+						<TextField label="Text Field" defaultValue="Medium" variant="outlined"/>
+						<TextField label="Text Field" defaultValue="Medium" variant="outlined"/>
+					</div>
+				</div>
+				<div style={{
+					display: 'flex', flexDirection: 'row', justifyContent: 'space-around',
+					alignItems: 'center', width: '100%'
+				}}>
+					<div style={{
+						display: 'flex', flexDirection: 'column', justifyContent: 'flex-start',
+						width: '50%', marginBottom: '20px'
+					}}>
+						<FormControl variant="outlined" style={{width: '28%', marginBottom: '20px'}}>
+							<InputLabel id="demo-simple-select-outlined-label">Dropdown</InputLabel>
+							<Select
+								labelId="demo-simple-select-outlined-label"
+								id="demo-simple-select-outlined"
+								value={10}
+								label="Dropdown"
+							>
+								<MenuItem value="">
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={10}>Ten</MenuItem>
+								<MenuItem value={20}>Twenty</MenuItem>
+								<MenuItem value={30}>Thirty</MenuItem>
+							</Select>
+						</FormControl>
+						<FormControl variant="outlined" style={{width: '28%', marginBottom: '20px'}}>
+							<InputLabel id="demo-simple-select-outlined-label">Dropdown</InputLabel>
+							<Select
+								labelId="demo-simple-select-outlined-label"
+								id="demo-simple-select-outlined"
+								value={10}
+								label="Dropdown"
+							>
+								<MenuItem value="">
+									<em>None</em>
+								</MenuItem>
+								<MenuItem value={10}>Ten</MenuItem>
+								<MenuItem value={20}>Twenty</MenuItem>
+								<MenuItem value={30}>Thirty</MenuItem>
+							</Select>
+						</FormControl>
+					</div>
+					<TextareaAutosize></TextareaAutosize>
+				</div>
+				<div></div>
+				<div></div>
+				<div><Button><Link to="/Admin">Admin Page</Link></Button></div>
+			</div>
+		)
 	}
 }
 
