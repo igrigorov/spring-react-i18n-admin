877cf7d1c51f332abdb867a92488985aa5d7fc1b
Labros Papadopoulos <lapapadopoulos@cognity.gr>
2021-05-13 14:28:00 +0300

    Centralize ui endpoint - update

--- i18n-ui/src/Networking/API/GetAll.js
+++ i18n-ui/src/Networking/API/GetAll.js
@@ -1,6 +1,9 @@
-export default function GetAll(url) {
+import {l10nServiceUrl} from '../../constants/endpoints'
+
+export default function GetAll() {
+	const url = l10nServiceUrl('/l10n')
 	try {
-		return fetch(url + "/l10n")
+		return fetch(url)
 			.then((response => response.json()))
 	} catch (e) {
 		throw e;
